1d811eaa45bc3d931d2f6ac6a9cd5a917d1c8a2f
albertoR96 <alberto.rochacrv96@gmail.com>
2022-11-02 15:05:39 -0600

    first commit

---
+++ save_pic.php
@@ -0,0 +1,58 @@
+<?php
+
+header('Content-type: text/plain');
+// Rui Santos
+// Complete project details at https://RandomNerdTutorials.com/esp32-cam-post-image-photo-server/
+// Code Based on this example: w3schools.com/php/php_file_upload.asp
+
+$target_dir = "C:\\wamp64\\www\\livecam\\uploads\\";
+$dateString = new DateTime();
+$target_file = $target_dir . $dateString->format('Y-m-d_H_i_s') . basename($_FILES["imageFile"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["imageFile"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  }
+  else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["imageFile"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["imageFile"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["imageFile"]["name"]). " has been uploaded.";
+  }
+  else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
